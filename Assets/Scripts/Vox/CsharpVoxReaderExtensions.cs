bd0c7d12cb7b3becd9ac1bbd1fb35ad3e2dfc2ae
Pavel Kouřil <pk@pavelkouril.cz>
2020-04-18 21:31:59 +0200

    added support for loading vox file format

---
+++ Assets/Scripts/Vox/CsharpVoxReaderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public static class UnityVoxReaderExtensions
+{
+    /// <summary>
+    /// Read a <see cref="Color32"/> stored in a <see cref="uint"/> in the CsharpVoxReader palette
+    /// </summary>
+    /// <param name="value">Input value</param>
+    public static Color32 ToColor(this uint value)
+    {
+        byte[] bytes = BitConverter.GetBytes(value);
+        return new Color32(bytes[2], bytes[1], bytes[0], bytes[3]);
+    }
+}
---
+++ Assets/Scripts/Vox/VoxLevelLoader.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CsharpVoxReader;
+using System;
+using CsharpVoxReader.Chunks;
+
+
+public class VoxLevelLoader : MonoBehaviour
+{
+    class MyLoader : IVoxLoader
+    {
+        public uint[] Palette { get; private set; }
+        public byte[,,] Data { get; private set; }
+        public Vector3Int Size { get; private set; }
+
+
+        public void LoadModel(int sizeX, int sizeY, int sizeZ, byte[,,] data)
+        {
+            Data = data;
+            Size = new Vector3Int(sizeX, sizeY, sizeZ);
+
+        }
+
+        public void LoadPalette(uint[] palette)
+        {
+            Palette = palette;
+        }
+
+        public void NewGroupNode(int id, Dictionary<string, byte[]> attributes, int[] childrenIds)
+        {
+        }
+
+        public void NewLayer(int id, Dictionary<string, byte[]> attributes)
+        {
+        }
+
+        public void NewMaterial(int id, Dictionary<string, byte[]> attributes)
+        {
+        }
+
+        public void NewShapeNode(int id, Dictionary<string, byte[]> attributes, int[] modelIds, Dictionary<string, byte[]>[] modelsAttributes)
+        {
+        }
+
+        public void NewTransformNode(int id, int childNodeId, int layerId, Dictionary<string, byte[]>[] framesAttributes)
+        {
+        }
+
+        public void SetMaterialOld(int paletteId, MaterialOld.MaterialTypes type, float weight, MaterialOld.PropertyBits property, float normalized)
+        {
+        }
+
+        public void SetModelCount(int count)
+        {
+        }
+    }
+
+    private void Start()
+    {
+        var loader = new MyLoader();
+        VoxReader reader = new VoxReader("Assets/Levels/level0.vox", loader);
+
+        reader.Read();
+
+        // DebugInput(loader);
+    }
+
+    private static void DebugInput(MyLoader loader)
+    {
+        for (int x = 0; x < loader.Size.x; x++)
+        {
+            for (int y = 0; y < loader.Size.y; y++)
+            {
+                for (int z = 0; z < loader.Size.z; z++)
+                {
+                    var idx = loader.Data[x, y, z];
+                    if (idx != 0)
+                    {
+                        var color = loader.Palette[idx].ToColor();
+
+                        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        //cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                        cube.transform.position = new Vector3(x, y, z);
+
+                        cube.GetComponent<MeshRenderer>().material.SetColor("_Color", color);
+                    }
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
